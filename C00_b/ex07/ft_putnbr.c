15157759b45a6555be8fd40aab7adc83c6ae9a8c
Marta Garzia <mgarzia@e3r5p8.42roma.it>
2024-11-14 14:25:46 +0100

    ex07 done

---
+++ C00_b/ex07/ft_putnbr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarzia <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 14:05:54 by mgarzia           #+#    #+#             */
+/*   Updated: 2024/11/14 14:24:25 by mgarzia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <limits.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar((nb % 10) + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(21);
+	return (0);
+}
