1ce7e361f7f8f09f7ccafba032792243640239c1
Marta Garzia <mgarzia@e3r1p6.42roma.it>
2024-11-23 18:43:30 +0100

    corrected C01 ex01

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -1,26 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarzia <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 17:57:56 by mgarzia           #+#    #+#             */
+/*   Updated: 2024/11/23 18:18:32 by mgarzia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-char    *ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 /*
 int main(void)
 {
-    char src[] = "Dolphins are cool";
-    char dest[20];
-
-    ft_strcpy(dest, src);
-    printf("%s\n", dest);
-    return (0);
+	char src[] = "Dolphins are cool";
+	char dest[20];	
+	ft_strcpy(dest, src);
+	printf("%s", dest);
+	return (0);
 }
 */
