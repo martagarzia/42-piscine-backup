1b6e4a5d41dc22d568a71f30a268b3624d086be5
Marta Garzia <mgarzia@e3r1p6.42roma.it>
2024-11-27 06:51:54 +0100

    C05 ex05

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -1,13 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgarzia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/27 05:53:17 by mgarzia           #+#    #+#             */
-/*   Updated: 2024/11/27 05:54:38 by mgarzia          ###   ########.fr       */
+/*   Created: 2024/11/27 06:44:14 by mgarzia           #+#    #+#             */
+/*   Updated: 2024/11/27 06:50:48 by mgarzia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	if (nb <= 0)
+		return (0);
+	i = 1;
+	while (i * i <= nb && i <= 46340)
+	{
+		if (i * i == nb)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	int	result;
+
+	result = ft_sqrt(16);
+	printf("%d", result);
+	return (0);
+}
+*/
